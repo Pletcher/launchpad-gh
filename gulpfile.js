e936bf772b74c0b45b6fdba8c18893432577f043
Charles Pletcher <cpletcher2@gmail.com>
2014-08-21 13:58:49 -0700

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
+var version = require('./package.json').version;
 
 // semantic versioning
 
@@ -8,6 +9,22 @@ var _bump = function(type) {
   gulp.src('./package.json')
   .pipe(bump({ type: type }))
   .pipe(gulp.dest('./'));
+
+  version = version.split('.');
+
+  switch(type) {
+    case 'patch':
+      version[2] = (parseInt(version[2]) + 1).toString();
+      break;
+    case 'minor':
+      version[1] = (parseInt(version[1]) + 1).toString();
+      break;
+    case 'major':
+      version[0] = (parseInt(version[0]) + 1).toString();
+      break;
+  }
+
+  version = version.join('.');
 };
 
 gulp.task('patch', function() {
@@ -25,17 +42,13 @@ gulp.task('major', function() {
 // git
 
 gulp.task('tag', function() {
-  var version = require('./package.json').version;
-
   git.tag('v' + version, 'Tag v' + version, function(err) {
     if (err) throw err;
   });
 });
 
 gulp.task('commit', function() {
-  var version = require('./package.json').version;
-
-  gulp.src('./*')
+  gulp.src('./package.json')
   .pipe(git.commit('Tag v' + version));
 });
 
@@ -47,7 +60,7 @@ gulp.task('push', function() {
 
 // release
 
-gulp.task('release', ['tag', 'commit', 'push']);
+gulp.task('release', ['commit', 'tag', 'push']);
 gulp.task('release-patch', ['patch', 'release']);
 gulp.task('release-minor', ['minor', 'release']);
 gulp.task('release-major', ['major', 'release']);
